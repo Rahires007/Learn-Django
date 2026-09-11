cdd48bf88b4fa2ef7e150bfdf528536c00fe2ef4
rahires007@gmail.com <rahires007@gmail.com>
2026-09-12 01:16:54 +0530

    Api Created successfully

--- Project06/Api_Creation/Api/views.py
+++ Project06/Api_Creation/Api/views.py
@@ -4,34 +4,119 @@ from rest_framework import status
 
 from .serializer import StudentSerializer
 from .models import Students
+
+
 # Create your views here.
-@api_view(["GET","POST","DELETE","PUT"])
+@api_view(["GET", "POST", "DELETE", "PUT", "PATCH"])   #HTTP method work on api 
 def Api_Create_View(request):
-    if request.method=="GET":   #Get Method
-        students=Students.objects.all() #Get all students from models & store in students
-        serializer=StudentSerializer(students,many=True)  #Serialize all students mean convert The queryset into json
-        return Response(serializer.data,status=status.HTTP_200_OK)  #Give Responce with Status code
-    elif request.method=="POST":
-        serializer=StudentSerializer(data=request.data)  #Extract data from request & serilize it 
-        if serializer.is_valid(): #Check data inside serilizer is valid or not 
-            serializer.save()   #Save data in the model
-            return Response(serializer.data,status=status.HTTP_201_CREATED)  #Responce with status code
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-    elif request.method=="DELETE":
-        Id=request.data.get("id")   #Extract id from request
-        student=Students.objects.get("id=Id")   #Find the student by using id
-        if student is not None:  #Student if present 
-            student.delete()   #
-            return Response(status=status.HTTP_204_NO_CONTENT)
+
+    # GET Method
+    if request.method == "GET":
+        students = Students.objects.all()   #Get all records from models & store
+        serializer = StudentSerializer(students, many=True)  #Serialize & store data
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )   #Give Responce
+
+    # POST Method
+    elif request.method == "POST":
+        serializer = StudentSerializer(data=request.data)   #Serialize & store data extract from request
+
+        if serializer.is_valid():  #Serialize data is valid 
+            serializer.save()   #save the data
+
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )  #Give Responce
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )    #Give Error Responce
+
+    # DELETE Method
+    elif request.method == "DELETE":
+        Id = request.data.get("id")   #Extract id from request 
+
+        student = Students.objects.filter(id=Id).first()  #Find Record using id & filter & first
+
+        if student is not None:   #Check it none
+            student.delete()   #Delete record
+
+            return Response(
+                status=status.HTTP_204_NO_CONTENT
+            )   # Give Responce
+
         else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-    elif request.method=="PUT":
-        Id=request.data.get("id")
-        student=Students.objects.get("id=Id")
-        if student is not None:
-            serializer=StudentSerializer(student,data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data,status=status.HTTP_201_CREATED)
+            return Response(
+                {"error": "Student not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )  #Give Error Responce
+
+    # PUT Method
+    elif request.method == "PUT":
+        Id = request.data.get("id") #Extract id from request
+
+        student = Students.objects.filter(id=Id).first()  #Find Record using id
+
+        if student is not None:  #Check it none
+            serializer = StudentSerializer(
+                student,
+                data=request.data
+            )   #Serialize data
+
+            if serializer.is_valid():  #Check Serializer data is valid
+                serializer.save()   #Save the data
+
+                return Response(
+                    serializer.data,
+                    status=status.HTTP_200_OK
+                )  #Give Responce
+
             else:
-                return Response(status=status.HTTP_400_BAD_REQUEST)        
+                return Response(
+                    serializer.errors,
+                    status=status.HTTP_400_BAD_REQUEST
+                )  #Give Error Responce
+
+        else:
+            return Response(
+                {"error": "Student not found"},
+                status=status.HTTP_404_NOT_FOUND
+            ) #Give Error Responce 
+
+    # PATCH Method
+    elif request.method == "PATCH":
+        Id = request.data.get("id")   #Extract Id
+
+        student = Students.objects.filter(id=Id).first()  #Find Record using id
+
+        if student is not None:
+            serializer = StudentSerializer(
+                student,
+                data=request.data,
+                partial=True
+            )   #Serialize & Store data
+
+            if serializer.is_valid():
+                serializer.save()  #Save data
+
+                return Response(
+                    serializer.data,
+                    status=status.HTTP_200_OK
+                )   #Give Responce
+
+            else:
+                return Response(
+                    serializer.errors,
+                    status=status.HTTP_400_BAD_REQUEST
+                )   #Give Error Responce
+
+        else:
+            return Response(
+                {"error": "Student not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )   #Give Error Responce
