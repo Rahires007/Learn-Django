5eb9dce004206543da3f808467e2a12026ea6d26
rahires007@gmail.com <rahires007@gmail.com>
2026-09-13 23:47:52 +0530

    Complete Employee Management System

--- Project04/Employee_Management_System/Employee/urls.py
+++ Project04/Employee_Management_System/Employee/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
 urlpatterns=[
-    path('',Dashboard),
-    path('Form/',Form)
+    path('',Dashboard,name="Dashboard"),
+    path('Form/',Form,name="Form"),
+    path('Delete/<int:pk>/',Delete,name="Delete"),
+    path('Update/<int:pk>/',Update,name="Update")
 ]
--- Project04/Employee_Management_System/Employee/views.py
+++ Project04/Employee_Management_System/Employee/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http import HttpResponse
 from .models import Employee_Management_System
 # Create your views here.
 def Dashboard(request):
@@ -11,3 +12,20 @@ def Form(request):
         Salary=request.POST.get("Salary")
         Employee_Management_System.objects.create(Name=Name,City=City,Salary=Salary)
     return render(request,'Employee/Form.html')
+def Delete(request,pk):
+    Employee=get_object_or_404(Employee_Management_System,pk=pk)
+    Employee.delete()
+    return redirect(Dashboard)
+def Update(request,pk):
+    Employee=get_object_or_404(Employee_Management_System,pk=pk)
+    if request.method=="POST":
+        Name=request.POST.get("Name")
+        City=request.POST.get("City")
+        Salary=request.POST.get("Salary")
+        Employee.Name=Name
+        Employee.City=City
+        Employee.Salary=Salary
+        Employee.save()
+        return redirect(Dashboard)
+    else:
+        return render(request,'Employee/Edit.html',{"Employee":Employee})
